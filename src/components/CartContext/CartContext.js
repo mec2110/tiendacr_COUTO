46b9e0e9d8bb340e72f9dfed078249fd36413a05
Mec <94048003+mec2110@users.noreply.github.com>
2021-12-27 23:45:48 -0300

    Update CartContext.js

--- src/components/CartContext/CartContext.js
+++ src/components/CartContext/CartContext.js
@@ -1,57 +1,62 @@
 import React, { createContext, useState } from "react";
 import "./CartContext.css";
-//import {useState } from "react";
-//import ItemCount from "./ItemCount";
 
 export const CartContext = createContext();
 
 const CartContextProvider = ({children}) => {
-const [cart, setCart] = useState([]);
-
-const getquantity = () => {
-    let subTotal = 0;
-    cart.forEach(i=> {
-        console.log(i);
-        subTotal += i.quantity
-    })
-    return subTotal;
-}
-
-
-
-const addItem = (product, Totalquantity) => {
-const flag = isInCart(product);
-if (flag) {
-        let productRepeat = cart.find (i => i.item === product);
-        productRepeat.quantity += Totalquantity;
-
-        let cartNoRepeat = cart.filter (i => i.item !== product); //todos los elementos que no sean el producto en cuestiòn
-        setCart([...cartNoRepeat, productRepeat]);
-    } else {
-        setCart([...cart, {item: product, quantity: Totalquantity}]);
+    const [cart, setCart] = useState([]);
+    
+    const getquantity = () => {
+        let subTotal = 0;
+        cart.forEach(i=> {
+            console.log(i);
+            subTotal += i.quantity
+        })
+        return subTotal;
     }
+    
+
+    const addItem = (product, Totalquantity) => {
+        const flag = isInCart(product);
+        if (flag) {
+                let productRepeat = cart.find (i => i.item === product);
+                productRepeat.quantity += Totalquantity;
+        
+                let cartNoRepeat = cart.filter (i => i.item !== product); //todos los elementos que no sean el producto en cuestiòn
+                setCart([...cartNoRepeat, productRepeat]);
+            } else {
+                setCart([...cart, {item: product, quantity: Totalquantity}]);
+            }
+    addQuantity();
 }
 
 const isInCart = (item) => {
     console.log(item);
-    return cart.some(products => products.item === item );
+    return cart.some(product => product.item === item );
 }
 
-const removeItem = (products) => {
+const removeItem = (itemId) => {
+console.log(item);
+const item = list.filter ((e)=>{
+    return parseInt (e.id) !== parseInt (itemId)
+})
+}
+updateCounter ();
 
-let removeFromCart = cart.filter (i => i.item !== products);
-setCart ([removeFromCart]);
+
+const cleanCart = (quantity) => {
+  console.log(quantity);
+  const items = list.filter ((e)=>{
+    return parseInt (e.id) !== parseInt (quantity)
+})
 }
 
-const cleanCart = () => {
- setCart ([]);
-}
 
- return (
-     <CartContext.Provider value ={{cart, getquantity, addItem, removeItem, cleanCart }} >
-         {children}
-    </CartContext.Provider>
- )
+return (
+    <CartContext.Provider value ={{cart, getquantity, addItem, removeItem, cleanCart }} >
+        {children}
+   </CartContext.Provider>
+)
 
 }
 export default CartContextProvider;
