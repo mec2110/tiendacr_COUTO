23a0f6851b5d9b770eb63925f1df52c0cf0a837e
Mec <94048003+mec2110@users.noreply.github.com>
2021-12-27 23:22:46 -0300

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,21 +5,37 @@ import {CartContext} from "./CartContext/CartContext"
 
 
 const Cart =() => {
-    const {cart} = useContext (CartContext);
-
+    const {cart,removeItem,cleanCart} = useContext (CartContext);
     
     return(
-        <div className="margin-top">
-        <h1 className="titulo"> Próximamente cart </h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                <th className="datos" scope="col">Foto del producto</th>
+                <th  className="datos" scope="col">Descripción</th>
+                <th  className="datos" scope="col">Cantidad</th>
+                <th> </th>
+                </tr>
+            </thead>
+            <tbody>
         {cart.map(product => {
                         return <tr>
-                            <td> {product.item.image}</td>
-                            <td> {product.item.description} </td>
-                            <td> {product.quantity} </td>
-                        </tr>
+                            <td> <img src={product.item.img} alt={product.name} className="itemImg"/></td>
+                            <td className="datos"> {product.item.description} </td>
+                            <td className="datos" > Total: {product.quantity} </td>
+                            <td className="datos" > Precio x unidad:$ {product.item.price} </td>
+                            <td> <button className="btn-itemcount1" onClick={removeItem}> Eliminar item</button></td>
+                            </tr>
             })}
-        </div>
+
+            
+            </tbody>
+            <tbody>
+            <button className="btn-itemcount1" onClick={cleanCart}> Cancelar compra </button> 
+             <button className="btn-itemcount1" onClick={cart}> Comprar </button> 
+            </tbody>
+        </table>
     )
 }
 
-export default Cart 
+export default Cart;
