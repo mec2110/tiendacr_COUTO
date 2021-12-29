3bd448cb6b9e348681f2960ec14a9b0d327ff850
Mec <94048003+mec2110@users.noreply.github.com>
2021-12-29 20:50:02 -0300

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -14,6 +14,8 @@ const Cart =(product) => {
                 <th className="datos" scope="col">Foto del producto</th>
                 <th  className="datos" scope="col">Descripción</th>
                 <th  className="datos" scope="col">Cantidad</th>
+                <th  className="datos" scope="col">Precio x unidad:</th>
+                <th  className="datos" scope="col">Total:</th>
                 <th> </th>
                 </tr>
             </thead>
@@ -23,17 +25,23 @@ const Cart =(product) => {
                             <td> <img src={product.item.img} alt={product.name} className="itemImg"/></td>
                             <td className="datos"> {product.item.description} </td>
                             <td className="datos" > Total: {product.quantity} </td>
-                            <td className="datos" > Precio x unidad:$ {product.item.price} </td>
-                            <td> <button className="btn-itemcount1" onClick={() => removeItem (product.id)}> X </button></td>
+                            <td className="datos" > $ {product.item.price} </td>
+                            <td className="datos" >  ${product.item.price * product.quantity}</td>
+                            <td> <button className="btn-itemcount1" onClick={() => removeItem (product.item.id)}> X </button></td>
                             </tr>
             })}
 
             
             </tbody>
-            <tbody>
-            <div className="datos"> $ {product.price * product.quantity} </div>
+            <tbody className="datos">
+                <span>
+            <div > TOTAL A ABONAR: </div>
+            <div> ${product.price * product.quantity} </div>
+            <div>
             <button className="btn-itemcount1" onClick={cleanCart}> Cancelar compra </button> 
              <button className="btn-itemcount1" onClick={cart}> Confirmar Compra </button> 
+             </div>
+             </span>
             </tbody>
         </table>
     )
